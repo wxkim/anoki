26d397ef010699bac642ac84edfc08dd4edff7c7
William Kim <73252368+wxkim@users.noreply.github.com>
2025-07-30 14:02:16 -0500

    App subdirectory

---
+++ App/Inc/awmf210.h
@@ -0,0 +1,59 @@
+#ifndef
+#define AWMF210_H
+
+#include "core_cm33.h"
+#include "main.h"
+#include "stdio.h"
+
+#define _PRINT_OVER_UART
+#ifdef _PRINT_OVER_UART
+
+//THIS IS A SYSCALL OVERRIDE
+
+int _write(int file, char *ptr, int len)
+{
+    for (int i = 0; i < len; i++) ITM_SendChar(*ptr++);
+    return len;
+}
+
+#endif
+
+typedef struct {
+    float angle_deg;
+} target_info_t; //TargetInfo
+
+
+typedef struct {
+    uint8_t lo_phase;
+    uint8_t if_phase;
+    uint8_t tx_gain;
+    uint8_t lo_phase_rx;
+    uint8_t if_phase_rx;
+    uint8_t rx_gain;
+} phase_settings_t; //PhaseSettings
+
+void awmf210_write(uint16_t reg_addr, uint64_t data);
+void awmf210_setIFPhaseTX(uint8_t phase_code);
+void awmf210_setLOPhaseTX(uint8_t phase_code);
+void awmf210_setTXGain(uint8_t gain_code);
+
+void awmf210_setIFPhaseRX(uint8_t phase_code);
+void awmf210_setLOPhaseRX(uint8_t phase_code);
+void awmf210_setRXGain(uint8_t gain_code);
+
+void awmf210_configTXPath(uint8_t lo_phase, uint8_t if_phase, uint8_t gain_code);
+void awmf210_configRXPath(uint8_t lo_phase, uint8_t if_phase, uint8_t gain_code);
+
+void awmf210_init(void);
+
+target_info_t getTargetInfo(void);
+
+phase_settings_t calculateBeamformingPhases(target_info_t *target);
+
+
+
+
+
+#endif
+
+
